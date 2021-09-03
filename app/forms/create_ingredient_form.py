ea8db263d89724ebf7df8a234d58f5dd7e6b2c13
meagan <meagan.h.smith@gmail.com>
2021-09-03 15:33:39 -0500

    Add backend validations

--- app/forms/create_ingredient_form.py
+++ app/forms/create_ingredient_form.py
@@ -1,13 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import TextField, IntegerField, SubmitField
+from wtforms import validators
 from wtforms.fields.core import FloatField
-from wtforms.validators import DataRequired, ValidationError
+from wtforms.validators import DataRequired, ValidationError, Length
 
 v = [DataRequired()]
 
 class IngredientForm(FlaskForm):
     amt = FloatField("amt")
     measurement = TextField("measurement")
-    ingredient_name = TextField("ingredient_name", v)
+    ingredient_name = TextField("ingredient_name", validators=[DataRequired(), Length(min=1, max=50, message="Ingredient much be 1-50 characters long.")])
     recipe_id = IntegerField("recipe_id", v)
     submit = SubmitField("Submit")
