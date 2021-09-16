d98342120e3f7f8e2fe03d82bd9b2bf558421265
meagan <meagan.h.smith@gmail.com>
2021-09-16 11:31:40 -0500

    Add recipes by category functionality

--- react-app/src/components/Categories/Categories.js
+++ react-app/src/components/Categories/Categories.js
@@ -3,14 +3,18 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom';
 import { Redirect } from 'react-router-dom';
 import { getRecipes } from '../../store/recipe';
+import './Categories.css'
 
 function Categories() {
     const dispatch = useDispatch();
     const history = useHistory();
 
-    const recipesArr = Object.values(useSelector(state => state?.recipes))
-    // const recipesByCategory = recipesArr?.filter(recipe => recipe?.category_id == categories?.id)
+    const [category_id, setCategory_id] = useState('0');
 
+    const recipesArr = Object.values(useSelector(state => state?.recipes))
+    const recipesByCategory = recipesArr?.filter(recipe => recipe?.category_id == category_id)
+
+    console.log("recipes by category", recipesByCategory)
     // console.log("recipesArr in Categories", recipesArr)
 
     useEffect(() => {
@@ -18,30 +22,36 @@ function Categories() {
         // {<Redirect to='/categories/1' />}
     }, [dispatch])
 
-    const [category_id, setCategory_id] = useState('0');
+
+    console.log("category_id", category_id)
+
     const selectCategoryId = (e) => setCategory_id(Number(e.target.value));
 
     console.log("category Id", category_id)
 
-    const handleCategorySelect = async(e, catId) => {
+    const handleChange = async(e) => {
+        setCategory_id(e.target.value)
+    }
+
+    const handleSubmit = async(e) => {
         e.preventDefault();
 
-        selectCategoryId(catId);
+        console.log("In the handle submit")
+        console.log("categoryId in the handleSubmit", category_id)
+        // selectCategoryId(e);
 
-        history.push(`/categories/${category_id}`)
+        // history.push(`/categories/${category_id}`)
     }
 
     return (
         <>
-            <h1>Test Page for Categories</h1>
-
-            <div>
-                <div className="category-div">
-                    <form className="category-select-form" onSubmit={handleCategorySelect}>
+            <div className="categories-page-div">
+                <div className="category-select-div">
+                    <form className="category-select-form" onSubmit={handleSubmit}>
                         {/* <button className="category-dropdown-button">Recipe Category</button>   */}
                         {/* <label>Recipe Category</label> */}
-                        <div className="category-content-div">
-                            <select id="create-recipe-select" className="category-select-list" value={category_id} onChange={(e) => handleCategorySelect(e, category_id)}>
+                        <div className="category-sort-content-div">
+                            <select id="create-category-select" className="category-select-list" value={category_id} onChange={handleChange}>
                                 <option selected disabled hidden value='0'>Select a Recipe Category</option>
                                 <option className="test" value='1'>Soups</option>
                                 <option className="test" value='2'>Salads</option>
@@ -54,7 +64,29 @@ function Categories() {
                                 <option selected disabled hidden value='9'>Category Submitted!</option>
                             </select>
                         </div>
+
+                        {/* <input className="category-select-button" type="submit" value="Submit" /> */}
+
                     </form>
+                </div>
+
+                <div className="display-category-recipes-div">
+                    { recipesByCategory.map(recipe => (
+                        <div className="main-page-recipe-img-title">
+                            <div className="food-img-div">
+                                <a href={`/recipes/${recipe.id}`} id={recipe.id}>
+                                <img className="food-img" alt="food item"
+                                    onError={(event) => event.target.setAttribute("src", "https://live.staticflickr.com/65535/51418222296_9c3fcb2090_w.jpg")}
+                                    src={ recipe.food_img } />
+                                </a>
+
+                            </div>
+                            <div className="all-recipes-food-title-div">
+                                <h3 className="all-recipes-food-title-text">{recipe.title}</h3>
+                            </div>
+                        </div>
+                    ))}
+
                 </div>
             </div>
         </>
